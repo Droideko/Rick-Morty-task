b56fe7634c8f551d3ffa1769fb72a4aea3c083ec
Droideka <vindou@mail.ru>
2019-12-21 03:33:51 +0300

    Creating pagination

--- js/main.js
+++ js/main.js
@@ -1,23 +1,48 @@
 let table = document.getElementById('pagination');
-let lists = document.querySelectorAll('.pagination__item');
+let prevPageButton = document.querySelector('.pagination__item-prev');
+let nextPageButton = document.querySelector('.pagination__item-next');
+let currentPage = document.querySelector('.pagination__item-active');
 let sortBtn = document.querySelector('.pagination__sort-button');
-const url = `https://rickandmortyapi.com/api/character/`
+let url = `https://rickandmortyapi.com/api/character/?page=1`;
+getPagination(url);
 async function getPagination(url){
    let response = await fetch(url);
    let responseJson = await response.json();
-   let nextPage = responseJson.info.next;
+   let prevPageUrl = responseJson.info.prev || null;
+   let nextPageUrl = responseJson.info.next || null;
    let characters = responseJson.results;
+   let pages = responseJson.info.pages;
+   let numberCurrentPage = url.match(/\d+/g)[0];
+   let currentUrl = url;
    getCharacters(characters);
    sortBtn.addEventListener('click', e => {
       characters = characters.sort((a, b) => a.name.charCodeAt(0) - b.name.charCodeAt(0));
       table.innerHTML = '';
       getCharacters(characters);
+   });  
+   currentPage.textContent = `Current Page ${numberCurrentPage}`;
+   nextPageButton.addEventListener('click', e => {
+      if (pages > numberCurrentPage){
+         currentPage.textContent = `Current Page ${++numberCurrentPage}`;
+         f(nextPageUrl);
+      }      
    });
+   prevPageButton.addEventListener('click', e => {
+      if (1 < numberCurrentPage){
+         currentPage.textContent = `Current Page ${--numberCurrentPage}`;
+         f(prevPageUrl);
+      }  
+   });
+   async function f(url){
+      response = await fetch(url);
+      responseJson = await response.json();
+      characters = responseJson.results;
+      prevPageUrl = responseJson.info.prev || null;
+      nextPageUrl = responseJson.info.next || null;
+      table.innerHTML = '';
+      getCharacters(characters);    
+   }
 }
-getPagination(url);
-
-
-
 
 function getCharacters(characters){
    characters.forEach(character => {
