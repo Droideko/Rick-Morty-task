7603713aac3a804bcdc164d501cbc2370fd1752e
Droideka <vindou@mail.ru>
2019-12-21 22:04:15 +0300

    creating modal windiw & fixing main.js

--- js/main.js
+++ js/main.js
@@ -1,38 +1,46 @@
-let table = document.getElementById('pagination');
-let prevPageButton = document.querySelector('.pagination__item-prev');
-let nextPageButton = document.querySelector('.pagination__item-next');
-let currentPage = document.querySelector('.pagination__item-active');
-let sortBtn = document.querySelector('.pagination__sort-button');
-let url = `https://rickandmortyapi.com/api/character/?page=1`;
+let table = document.getElementById('pagination'),
+    modal = document.querySelector('.modal'),
+    modalContainer = document.querySelector('.modal-container'),
+    closeModal = document.querySelector('.modal-container__close'),
+    prevPageButton = document.querySelector('.pagination__item-prev'),
+    nextPageButton = document.querySelector('.pagination__item-next'),
+    currentPage = document.querySelector('.pagination__item-active'),
+    sortBtn = document.querySelector('.pagination__sort-button'),
+    url = `https://rickandmortyapi.com/api/character/?page=1`;
+
 getPagination(url);
+
 async function getPagination(url){
    let response = await fetch(url);
    let responseJson = await response.json();
-   let prevPageUrl = responseJson.info.prev || null;
-   let nextPageUrl = responseJson.info.next || null;
-   let characters = responseJson.results;
-   let pages = responseJson.info.pages;
-   let numberCurrentPage = url.match(/\d+/g)[0];
-   let currentUrl = url;
+   let [prevPageUrl, nextPageUrl] = [responseJson.info.prev || null, responseJson.info.next || null];
+   let characters = responseJson.results,
+       pages = responseJson.info.pages,
+       numberCurrentPage = url.match(/\d+/g)[0],
+       currentUrl = url;
+   currentPage.textContent = `Current Page ${numberCurrentPage}`;
    getCharacters(characters);
+
    sortBtn.addEventListener('click', e => {
       characters = characters.sort((a, b) => a.name.charCodeAt(0) - b.name.charCodeAt(0));
       table.innerHTML = '';
       getCharacters(characters);
    });  
-   currentPage.textContent = `Current Page ${numberCurrentPage}`;
+   
    nextPageButton.addEventListener('click', e => {
       if (pages > numberCurrentPage){
          currentPage.textContent = `Current Page ${++numberCurrentPage}`;
          f(nextPageUrl);
       }      
    });
+
    prevPageButton.addEventListener('click', e => {
       if (1 < numberCurrentPage){
          currentPage.textContent = `Current Page ${--numberCurrentPage}`;
          f(prevPageUrl);
       }  
    });
+
    async function f(url){
       response = await fetch(url);
       responseJson = await response.json();
@@ -45,22 +53,47 @@ async function getPagination(url){
 }
 
 function getCharacters(characters){
-   characters.forEach(character => {
-      let name = character.name;
-      let imageLink = character.image;
-      createRowBlock(name, imageLink);
-   });      
+   characters.forEach(character => createRowBlock(character.name, character.image, character));      
 }
 
-function createRowBlock(name, imageLink){
-   let row = document.createElement('tr');
-   let img = document.createElement('img');
+function createRowBlock(name, imageLink, character){
+   let [row, img] = [document.createElement('tr'), document.createElement('img')];
    [img.src, img.alt] = [imageLink, name];
-   let imageCol = document.createElement('td');
+   img.addEventListener('click', () => createModal(character));
+   let [imageCol, nameCol] = [document.createElement('td'), document.createElement('td')]
    imageCol.append(img); 
-   let nameCol = document.createElement('td');
    nameCol.textContent = name;
    row.append(imageCol, nameCol);
    table.append(row);
 }
+
+function createModal(obj){
+   if (modalContainer.lastChild.tagName === 'DIV') modalContainer.lastChild.remove();
+   let container = document.createElement('div');
+   container.classList.add('modal-content');
+   let image = document.createElement('img');
+   [image.src, image.alt] = [obj.image, obj.name];
+   let modalInfo = document.createElement('div');
+   modalInfo.classList.add('modal-content__info');
+   let title = document.createElement('h4');
+   title.textContent = obj.name;
+   modalInfo.append(title);
+   let info = ['status', 'species', 'gender'];
+   for (let i = 0; i < info.length; i++){
+      let p = document.createElement('p');
+      p.textContent = `${info[i]}: ${obj[info[i]]}`;
+      modalInfo.append(p);
+   }
+   container.append(image);
+   container.append(modalInfo);
+   modalContainer.append(container);
+   modal.style.display = 'block';
+}
+
+window.addEventListener('click', e => {
+   if (e.target == modal) modal.style.display = 'none';
+});
+closeModal.addEventListener('click', () => modal.style.display = 'none');
+
+
 
